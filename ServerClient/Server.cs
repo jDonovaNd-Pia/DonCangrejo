b5b4e0d9881c60deb36183d5909f7e13cc1d89df
escola pia granollers <>
2023-10-17 20:40:23 +0200

    otro

--- ServerClient/Server.cs
+++ ServerClient/Server.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerClient
 {
@@ -10,6 +9,7 @@ namespace ServerClient
     {
         static void Main(string[] args)
         {
+           
         }
     }
 }
