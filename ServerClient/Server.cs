872fd31fd6e8a1da4ae747f4c570d0ea17c87d52
Aleix Soler Manuel <aleix.soler@epia.granollers>
2023-10-17 20:38:32 +0200

    Server part

--- ServerClient/Server.cs
+++ ServerClient/Server.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace ServerClient
 {
@@ -10,6 +9,78 @@ namespace ServerClient
     {
         static void Main(string[] args)
         {
+            TcpListener listener = new TcpListener(7777);
+            listener.Start();
+            
+            bool End = false;
+            bool reading = false;
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+
+            BinaryReader br = new BinaryReader(ns);
+
+            BinaryWriter bw = new BinaryWriter(ns);
+
+            while (End == false)
+            {
+                try
+                {
+                    if (reading)
+                    {
+                        byte b = br.ReadByte();
+                        if (b == 4)
+                        {
+                            int msg = br.ReadInt32();
+                            Console.WriteLine("int " + msg);
+                        }
+                        else if (b == 1)
+                        {
+                            byte msg = br.ReadByte();
+                            Console.WriteLine("byte " + msg);
+                        }
+                        else if (b == 2)
+                        {
+                            bool msg = br.ReadBoolean();
+                            Console.WriteLine("Bool " + msg);
+                        }
+                        reading = false;
+                    }
+                    else
+                    {
+                        string text = Console.ReadLine();
+                        if (text == "int")
+                        {
+                            byte size = 4;
+                            bw.Write(size);
+                            int msg = 69;
+                            bw.Write(msg);
+                        }
+                        else if (text == "byte")
+                        {
+                            byte size = 1;
+                            bw.Write(size);
+                            byte msg = 5;
+                            bw.Write(msg);
+                        }
+                        else if (text == "boolean")
+                        {
+                            byte size = 2;
+                            bw.Write(size);
+                            int msg = 1;
+                            bw.Write(msg);
+                        }
+                        bw.Flush();
+                        reading = true;
+                    }
+                }
+                catch(Exception e)
+                {
+                    End = true;
+                }                          
+            }
+                listener.Stop();
         }
     }
 }
+
+
